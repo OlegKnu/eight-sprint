1e3d4c5c6e58b4eca8a998436b203b4a66005918
Daniil, Kholkin <danyakholkin@yandex.ru>
2025-09-30 18:23:33 +0300

    third commit

--- parcel_test.go
+++ parcel_test.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"github.com/stretchr/testify/assert"
 	"math/rand"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
 	_ "modernc.org/sqlite"
 )
 
@@ -33,7 +33,7 @@ func getTestParcel() Parcel {
 func TestAddGetDelete(t *testing.T) {
 	// prepare
 	db, err := sql.Open("sqlite", "tracker.db") // подключение к БД
-	require.NoError(t, err)
+	assert.NoError(t, err)
 	defer db.Close()
 
 	store := NewParcelStore(db)
@@ -41,29 +41,29 @@ func TestAddGetDelete(t *testing.T) {
 
 	// add
 	id, err := store.Add(parcel)
-	require.NoError(t, err)
-	require.NotZero(t, id)
+	assert.NoError(t, err)
+	assert.NotZero(t, id)
 
 	// get
 	stored, err := store.Get(id)
-	require.NoError(t, err)
-	require.Equal(t, parcel.Client, stored.Client)
-	require.Equal(t, parcel.Status, stored.Status)
-	require.Equal(t, parcel.Address, stored.Address)
+	assert.NoError(t, err)
+	assert.Equal(t, parcel.Client, stored.Client)
+	assert.Equal(t, parcel.Status, stored.Status)
+	assert.Equal(t, parcel.Address, stored.Address)
 
 	// delete
 	err = store.Delete(id)
-	require.NoError(t, err)
+	assert.NoError(t, err)
 
 	_, err = store.Get(id)
-	require.Error(t, err) // посылка должна быть удалена
+	assert.Error(t, err) // посылка должна быть удалена
 }
 
 // TestSetAddress проверяет обновление адреса
 func TestSetAddress(t *testing.T) {
 	// prepare
 	db, err := sql.Open("sqlite", "tracker.db")
-	require.NoError(t, err)
+	assert.NoError(t, err)
 	defer db.Close()
 
 	store := NewParcelStore(db)
@@ -71,25 +71,25 @@ func TestSetAddress(t *testing.T) {
 	// add
 	parcel := getTestParcel()
 	id, err := store.Add(parcel)
-	require.NoError(t, err)
-	require.NotZero(t, id)
+	assert.NoError(t, err)
+	assert.NotZero(t, id)
 
 	// set address
 	newAddress := "new test address"
 	err = store.SetAddress(id, newAddress)
-	require.NoError(t, err)
+	assert.NoError(t, err)
 
 	// check
 	stored, err := store.Get(id)
-	require.NoError(t, err)
-	require.Equal(t, newAddress, stored.Address)
+	assert.NoError(t, err)
+	assert.Equal(t, newAddress, stored.Address)
 }
 
 // TestSetStatus проверяет обновление статуса
 func TestSetStatus(t *testing.T) {
 	// prepare
 	db, err := sql.Open("sqlite", "tracker.db")
-	require.NoError(t, err)
+	assert.NoError(t, err)
 	defer db.Close()
 
 	store := NewParcelStore(db)
@@ -97,24 +97,24 @@ func TestSetStatus(t *testing.T) {
 	// add
 	parcel := getTestParcel()
 	id, err := store.Add(parcel)
-	require.NoError(t, err)
-	require.NotZero(t, id)
+	assert.NoError(t, err)
+	assert.NotZero(t, id)
 
 	// set status
 	err = store.SetStatus(id, ParcelStatusSent)
-	require.NoError(t, err)
+	assert.NoError(t, err)
 
 	// check
 	stored, err := store.Get(id)
-	require.NoError(t, err)
-	require.Equal(t, ParcelStatusSent, stored.Status)
+	assert.NoError(t, err)
+	assert.Equal(t, ParcelStatusSent, stored.Status)
 }
 
 // TestGetByClient проверяет получение посылок по идентификатору клиента
 func TestGetByClient(t *testing.T) {
 	// prepare
 	db, err := sql.Open("sqlite", "tracker.db")
-	require.NoError(t, err)
+	assert.NoError(t, err)
 	defer db.Close()
 
 	store := NewParcelStore(db)
@@ -135,8 +135,8 @@ func TestGetByClient(t *testing.T) {
 	// add
 	for i := 0; i < len(parcels); i++ {
 		id, err := store.Add(parcels[i])
-		require.NoError(t, err)
-		require.NotZero(t, id)
+		assert.NoError(t, err)
+		assert.NotZero(t, id)
 
 		parcels[i].Number = id
 		parcelMap[id] = parcels[i]
@@ -144,15 +144,15 @@ func TestGetByClient(t *testing.T) {
 
 	// get by client
 	storedParcels, err := store.GetByClient(client)
-	require.NoError(t, err)
-	require.Len(t, storedParcels, len(parcels))
+	assert.NoError(t, err)
+	assert.Len(t, storedParcels, len(parcels))
 
 	// check
 	for _, parcel := range storedParcels {
 		expected, ok := parcelMap[parcel.Number]
-		require.True(t, ok)
-		require.Equal(t, expected.Client, parcel.Client)
-		require.Equal(t, expected.Status, parcel.Status)
-		require.Equal(t, expected.Address, parcel.Address)
+		assert.True(t, ok)
+		assert.Equal(t, expected.Client, parcel.Client)
+		assert.Equal(t, expected.Status, parcel.Status)
+		assert.Equal(t, expected.Address, parcel.Address)
 	}
 }
